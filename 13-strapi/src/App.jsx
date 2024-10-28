ac3381ca3e81cc723ec11c363e7a2c9b68c20ce4
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-10-29 04:38:57 +0800

    deployment fix

--- 13-strapi/src/App.jsx
+++ 13-strapi/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Sidebar from './components/Sidebar';
 import Submenu from './components/Submenu';
